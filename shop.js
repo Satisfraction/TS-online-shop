8fe8dfcd45429c1c00d00cfe1615b2b3adfc3f1a
Satisfraction <mathias.lappe@docc.techstarter.de>
2023-05-02 11:01:39 +0200

    Update
    update to 30 boxes of products
    some style changes

--- shop.js
+++ shop.js
@@ -4,60 +4,61 @@ const plusButtons = document.querySelectorAll('.plus-button');
 const minusButtons = document.querySelectorAll('.minus-button');
 
 for (let i = 0; i < plusButtons.length; i++) {
-    plusButtons[i].addEventListener('click', updateQuantity);
+	plusButtons[i].addEventListener('click', updateQuantity);
 }
 for (let i = 0; i < minusButtons.length; i++) {
-    minusButtons[i].addEventListener('click', updateQuantity);
+	minusButtons[i].addEventListener('click', updateQuantity);
 }
 
 function updateQuantity(event) {
-    const button = event.target;
-    const box = button.parentElement.parentElement;
-    const quantityInput = box.querySelector('input');
-    const price = box.querySelector('.price').textContent;
-    const priceValue = parseFloat(price.replace(',', '.'));
-    let quantityValue = parseInt(quantityInput.value);
+	const button = event.target;
+	const box = button.parentElement.parentElement;
+	const quantityInput = box.querySelector('input');
+	const price = box.querySelector('.price').textContent;
+	const priceValue = parseFloat(price.replace(',', '.'));
+	let quantityValue = parseInt(quantityInput.value);
 
-    if (button.classList.contains('plus-button')) {
-        quantityValue++;
-    } else if (button.classList.contains('minus-button')) {
-        quantityValue--;
-        if (quantityValue < 0) {
-            quantityValue = 0;
-            priceValue = 0;
-        }
-    }
+	if (button.classList.contains('plus-button')) {
+		quantityValue++;
+	} else if (button.classList.contains('minus-button')) {
+		quantityValue--;
+		if (quantityValue < 0) {
+			quantityValue = 0;
+			priceValue = 0;
+		}
+	}
 
-    quantityInput.value = quantityValue;
+	quantityInput.value = quantityValue;
 
-    const totalPriceElement = document.getElementById('total-price');
-    let totalPrice = 0;
-    for (let i = 0; i < quantityInputs.length; i++) {
-        const box = quantityInputs[i].parentElement.parentElement;
-        const quantityValue = parseInt(quantityInputs[i].value);
-        const price = box.querySelector('.price').textContent;
-        const priceValue = parseFloat(price.replace(',', '.'));
-        totalPrice += quantityValue * priceValue;
-    }
-    totalPriceElement.textContent = totalPrice.toFixed(2).replace('.', ',') + ' €';
+	const totalPriceElement = document.getElementById('total-price');
+	let totalPrice = 0;
+	for (let i = 0; i < quantityInputs.length; i++) {
+		const box = quantityInputs[i].parentElement.parentElement;
+		const quantityValue = parseInt(quantityInputs[i].value);
+		const price = box.querySelector('.price').textContent;
+		const priceValue = parseFloat(price.replace(',', '.'));
+		totalPrice += quantityValue * priceValue;
+	}
+	totalPriceElement.textContent = totalPrice.toFixed(2).replace('.', ',') + ' €';
 }
 
 function fetchProduct(id) {
 	return fetch(`https://dummyjson.com/products/${id}`)
-	  .then((res) => res.json());
-  }
-  
-  function updateProduct(product, idPrefix) {
+		.then((res) => res.json());
+}
+
+function updateProduct(product, idPrefix) {
 	const titleEl = document.getElementById(`${idPrefix}-title`);
-    const descriptionEl = document.getElementById(`${idPrefix}-description`);
+	const descriptionEl = document.getElementById(`${idPrefix}-description`);
 	const priceEl = document.getElementById(`${idPrefix}-price`);
 	const imageEl = document.getElementById(`${idPrefix}-image`);
-  
+
 	titleEl.textContent = product.title;
-    descriptionEl.textContent = product.description;
+	descriptionEl.textContent = product.description;
 	priceEl.textContent = product.price.toFixed(2) + '€';
 	imageEl.src = product.thumbnail;
-  }
+}
+
 
 fetchProduct(1)
 	.then((product) => {
@@ -214,6 +215,86 @@ fetchProduct(1)
     fetchProduct(20)
 	.then((product) => {
 	  updateProduct(product, 'product-20');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(21)
+	.then((product) => {
+	  updateProduct(product, 'product-21');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(22)
+	.then((product) => {
+	  updateProduct(product, 'product-22');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(23)
+	.then((product) => {
+	  updateProduct(product, 'product-23');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(24)
+	.then((product) => {
+	  updateProduct(product, 'product-24');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(25)
+	.then((product) => {
+	  updateProduct(product, 'product-25');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(26)
+	.then((product) => {
+	  updateProduct(product, 'product-26');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(27)
+	.then((product) => {
+	  updateProduct(product, 'product-27');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(28)
+	.then((product) => {
+	  updateProduct(product, 'product-28');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(29)
+	.then((product) => {
+	  updateProduct(product, 'product-29');
+	})
+	.catch((error) => {
+	  console.error('Fehler beim Abrufen des Produkts:', error);
+	});
+
+	fetchProduct(30)
+	.then((product) => {
+	  updateProduct(product, 'product-30');
 	})
 	.catch((error) => {
 	  console.error('Fehler beim Abrufen des Produkts:', error);
